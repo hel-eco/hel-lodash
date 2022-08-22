10e3c995758da432b68f7041562937b08ce2d098
fantasticsoul <zhongzhengkai@gmail.com>
2022-08-22 21:50:15 +0800

    feat:bump to ver 2

--- src/utils/myMod.ts
+++ src/utils/myMod.ts
@@ -1,5 +1,5 @@
 
 export function sayHelloToHel(from: string) {
   const yourRtxName = ''; // 此处改写为你的rtx名字
-  return `hello hel, i come from remote webpack app .... hel-micro world ..`;
+  return `hello hel, i come from remote the latest version .... x 1 ..`;
 }

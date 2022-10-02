ad9f23cc9faa557885430e57cb04ab048bd2e1f0
fantasticsoul <zhongzhengkai@gmail.com>
2022-10-02 12:12:41 +0800

    feat:bump hel-lib-proxy

--- src/utils/myMod.ts
+++ src/utils/myMod.ts
@@ -1,5 +1,6 @@
 
 export function sayHelloToHel(from: string) {
   const yourRtxName = ''; // 此处改写为你的rtx名字
-  return `hello hel, i come from remote the latest version .... x 1 ..`;
+  // return `hello hel, i come from remote the latest version .... x 1 ..`;
+  return `hello feishu world`;
 }

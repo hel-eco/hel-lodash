e09caadbcb1afdd69652777f1a62ae43bd00548b
fantasticsoul <zhongzhengkai@gmail.com>
2022-10-19 17:45:08 +0800

    feat:新增unpkg私服配置示例

--- config/subApp.js
+++ config/subApp.js
@@ -3,4 +3,7 @@ const pkg = require('../package.json');
 
 const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg' });
 
+// unpkg 私服
+// const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg', homePage: 'http://my-pack:8888' });
+
 module.exports = subApp;
